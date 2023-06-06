4413adf50e7f0751ab721f5d253143861d7345f3
randall.erasmus <randall.erasmus@interfront.co.za>
2023-06-06 23:53:22 +0200

    Rest Api basics configured

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,15 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+
 }
---
+++ src/main/java/org/example/WelcomeController.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String hello(){
+        return "Welcome to the backend";
+    }
+}
